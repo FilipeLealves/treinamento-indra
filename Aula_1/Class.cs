ba744d0d93daf543a28b8d7d5902aef4e9b20eb3
lealv <lealv@THEBEAST>
2022-04-23 20:13:04 -0300

    Adicionar arquivos de projeto.

---
+++ Aula_1/Class.cs
@@ -0,0 +1,15 @@
+﻿namespace Aula_1
+{
+    public class Bolo
+    {
+        List<Ingrediente> Ingredientes[];
+    }
+
+    public class Ingrediente
+    {
+        string Name;
+        string Cor;
+        float Peso;
+        double Volume;
+    }
+}
---
+++ Aula_1/Program.cs
@@ -0,0 +1,9 @@
+﻿using Aula_1;
+public class Program {
+    public static void Main(string[] args)
+    {
+        Bolo bolo;
+        bolo = new Bolo();
+
+    }
+}
